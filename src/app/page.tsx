a0177e3ad75a7c48a7d91d2940fc38ec2081b9cf
FustinCS <FustinCS@gmail.com>
2024-11-25 20:15:15 -0800

    Add blue theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/toggle-theme";
 
 export default function Home() {
   return (
     <div>
       <Button>Test Button</Button>
+      <ThemeToggle />
     </div>
   );
 }
